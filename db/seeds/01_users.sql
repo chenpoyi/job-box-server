4e4add463bcd37707a92317081e2a002b1f9666c
Paul Chen <paulchen647@gmail.com>
2020-12-21 16:43:23 +0800

    Set up database. Create users table and seeds

---
+++ db/seeds/01_users.sql
@@ -0,0 +1,4 @@
+insert into Users (name, email, password) values ('Sayer', 'sbirnie0@ihg.com', 'qcyq50');
+insert into Users (name, email, password) values ('Gray', 'gbezarra1@ning.com', 'pDrKxTykMD');
+insert into Users (name, email, password) values ('Dame', 'dmilillo2@blogtalkradio.com', 'W10vyDyqg3bY');
+insert into Users (name, email, password) values ('Rosalind', 'rmanclark3@google.ru', 'l04unBZ');
--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+require('dotenv').config();
 const express = require('express')
-const app = express()
-const port = 3000
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res) => {
+const cors = require('cors')
+const app = express()
+const port = 3001
+
+// PG database client/connection setup
+const { Pool } = require('pg');
+const dbParams = require('./lib/db.js');
+const db = new Pool(dbParams);
+db.connect();
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+app.use(cors())
+app.get('/api', (req, res) => {
+  console.log("RECEIVED")
   res.send('Hello World!')
 })
 
